56fe57134626372ff5b925a5d0c8101cc1c0cb64
Sammy Herring <sammy.herring@me.com>
2018-02-26 16:27:20 +0000

    Lab 6 Setup.

---
+++ Lab 6 - Exercise 2 Basic Meth/Lab 6 - Exercise 2 Basic Meth/Program.cs
@@ -0,0 +1,21 @@
+﻿//Project Name: Lab 6 - Exercise 2 Basic Meth | File Name: Program.cs
+//Author Name: Samuel Steven David Herring
+//Author URI: http://sherring.me
+//UserID: sh1042
+//Created On: 26/2/2018 | 16:26
+//Last Updated On:  26/2/2018 | 16:27
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6___Exercise_2_Basic_Meth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
